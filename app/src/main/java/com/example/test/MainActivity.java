90e5e82b1e137743db88b0113963a11b18e4bb44
erdongchen <568952575@qq.com>
2017-01-13 10:30:32 +0800

    --left

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
+private  void init(){
+    text= (TextView) findViewById(R.id.text);
+    text.setText("这是我们的大海");
+}
 }
